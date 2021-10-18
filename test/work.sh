682b0818ca411d42a30c4fd2fe1a6fb457d5ea24
Spartanzhao <zhao_yue000@163.com>
2021-10-18 11:08:40 +0800

    Add files via upload

---
+++ test/work.sh
@@ -0,0 +1,5 @@
+/absolute/path/of//python /absolute/path/of//Annoroad_Lego_Browser.py -fl show.list -o show.list1.pdf -chr chr9 -st 21559668  -ed 21561668  -b 4000000
+/absolute/path/of//python /absolute/path/of//Annoroad_Lego_Browser.py -fl show.list2 -o show.list2.pdf -chr chr7 -st 91000000 -ed 92000000 -b1 4000000 -b2 4000000
+/absolute/path/of//python /absolute/path/of//Annoroad_Lego_Browser.py -fl show.list3 -o show.list3.pdf -chr chr7 -st 91000000 -ed 92000000 -b1 4000000 -b2 4000000
+/absolute/path/of//python /absolute/path/of//Annoroad_Lego_Browser.py -fl show.list4 -o show.list4.pdf -chr chr7 -st 91000000 -ed 92000000 -b1 4000000 -b2 4000000
+/absolute/path/of//python /absolute/path/of//Annoroad_Lego_Browser.py -fl show.list5 -o show.list5.pdf -chr chr9 -st 117428792 -ed 118353967 -b1 1500000 -b2 1500000
